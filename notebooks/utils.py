069d3997f6e3210953b1c37cd541812b26ea7a46
amandavinci <amanthevinci@gmail.com>
2019-07-15 09:48:26 +0530

    add lr finder callback

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -128,9 +128,9 @@ class Callback:
         if cb_func and cb_func(): return True
         return False
 
-class CancelTrainExpection(Exception): pass
-class CancelEpochExpection(Exception): pass
-class CancelBatchExpection(Exception): pass
+class CancelTrainException(Exception): pass
+class CancelEpochException(Exception): pass
+class CancelBatchException(Exception): pass
 
 class Controller:
     '''Main Controller responsible for callbacks and training loop'''
@@ -161,14 +161,14 @@ class Controller:
             self.opt.step()
             if self('after_step'): return
             self.opt.zero_grad()
-        except CancelBatchExpection: self('after_cancel_batch')
+        except CancelBatchException: self('after_cancel_batch')
         finally:                     self('after_batch')
         
     def run_all_batches(self, dl):
         self.iters = len(dl)
         try:
             for xb, yb in dl: self.run_one_batch(xb, yb)
-        except CancelEpochExpection: self('after_cancel_epoch')
+        except CancelEpochException: self('after_cancel_epoch')
     
     def train(self, learner, epochs):
         self.learner, self.epochs = learner, epochs
@@ -185,7 +185,7 @@ class Controller:
                     if not self('before_validate'):
                         self.run_all_batches(self.data.test_dl)
                 if self('after_epoch'): break
-        except CancelTrainExpection: self('after_cancel_train') 
+        except CancelTrainException: self('after_cancel_train') 
         finally:                     self('after_train')
     
     def __call__(self, cb_func_name):
@@ -206,7 +206,7 @@ class Test(Callback):
         self.count += 1
         print(self.count)
         if self.count >= self.stop_at:
-            raise CancelTrainExpection()
+            raise CancelTrainException()
     
     def after_cancel_train(self):
         print(f"Training has been stopped by {self.__class__.__name__} callback")
@@ -272,3 +272,120 @@ class StatsReporter(Callback):
         train_str = f"Train loss: {train_avg_loss:.3f} \t metrics: {train_avg_metrics} \t"
         valid_str = f"Valid loss: {valid_avg_loss:.3f} \t metrics: {valid_avg_metrics} \t"
         print(header + train_str + valid_str)
+
+
+# ---
+# 06_Hyperparameter-Scheduling-with-Callbacks
+# ---
+
+import matplotlib.pyplot as plt
+
+class Recorder(Callback):
+    '''A callback to record the learning rate and loss after each training batch'''
+    
+    def before_train(self):
+        self.lrs = [[] for _ in self.opt.param_groups]
+        self.losses = []
+    
+    def after_batch(self):
+        if not self.in_train: return
+        for p_group, lr in zip(self.opt.param_groups, self.lrs):
+            lr.append(p_group['lr'])
+        self.losses.append(self.loss.detach().cpu())
+    
+    def plot_lrs(self, param_group_id=-1):
+        plt.plot(self.lrs[param_group_id])
+    
+    def plot_losses(self):
+        plt.plot(self.losses)
+    
+    def plot(self, param_group_id=-1, skip_last=0):
+        losses = [o.item() for o in self.losses]
+        lrs = self.lrs[param_group_id]
+        n = len(losses) - skip_last
+        plt.xscale('log')
+        plt.plot(lrs[:n], losses[:n])
+        plt.xlabel('Learning Rates')
+        plt.ylabel('Loss in Log Scale')
+        plt.title('Learning Rate vs Loss')
+
+
+class ParamScheduler(Callback):
+    """
+    Callback for scheduling any hyperparameter
+    registered in the state dict of the optimizer
+    """
+    
+    _order = 1
+    
+    def __init__(self, param_name, sched_funcs):
+        self.pname, self.sched_funcs = param_name, sched_funcs
+    
+    def before_train(self):
+        if not isinstance(self.sched_funcs, (list, tuple)):
+            self.sched_funcs = [self.sched_funcs] * len(self.opt.param_groups)
+    
+    def set_param(self):
+        assert len(self.opt.param_groups) == len(self.sched_funcs)
+        for param_group, func in zip(self.opt.param_groups, self.sched_funcs):
+            pos = self.n_epochs/self.epochs
+            param_group[self.pname] = func(pos)
+    
+    def before_batch(self):
+        if self.in_train: self.set_param()
+
+
+def scheduler(f):
+    def _inner(start, end): return partial(f, start, end)
+    return _inner
+
+@scheduler
+def constant(start, end, pos):    return start
+
+@scheduler
+def linear(start, end, pos): return start + (end-start)*pos
+
+@scheduler
+def exponential(start, end, pos): return start * (end/start)**pos
+
+@scheduler
+def cosine(start, end, pos):      return start + (1 + math.cos(math.pi*(1-pos))) * (end-start)/2
+
+def mixed_schedulers(pcts, schedulers):
+    assert sum(pcts) == 1
+    pcts = torch.tensor([0] + pcts)
+    pcts = torch.cumsum(pcts, dim=0)
+    def _inner(pos):
+        idx = (pos > pcts).nonzero().max()
+        lower_bound, upper_bound = pcts[idx], pcts[idx+1]
+        relative_pos = (pos - lower_bound) / (upper_bound-lower_bound)
+        return schedulers[idx](relative_pos)
+    return _inner
+
+
+class LR_Finder(Callback):
+    """
+    Finding the best learning rate
+    by varying it between reasonable bounds
+    and picking the one with the best loss
+    """
+    
+    def __init__(self, min_lr=1e-6, max_lr=10, max_iters=100):
+        self.min_lr, self.max_lr, self.max_iters = min_lr, max_lr, max_iters
+        self.best_loss = 1e9
+    
+    def before_batch(self):
+        if not self.in_train: return 
+        i = self.n_iter/self.max_iters
+        lr = self.min_lr * (self.max_lr/self.min_lr)**i
+        for group in self.opt.param_groups:
+            group['lr'] = lr
+    
+    def after_step(self):
+        if self.n_iter > self.max_iters or self.loss > self.best_loss*10:
+            raise CancelTrainException()
+        if self.loss < self.best_loss: self.best_loss = self.loss
+    
+    def after_cancel_train(self):
+        print(f"LR Finder found best loss of {self.best_loss:.4f}") 
+        print("Plot the 'Learning Rate VS Loss' curve to find the best learning rate")
